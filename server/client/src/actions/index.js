afb50d5f7e136cef9d0a8338f913bd1c2786d616
Ivelin Ivanov <ivelin.ivanovxps@gmail.com>
2017-11-05 20:08:35 +0100

    added payment method to client

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,8 +1,25 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+/**
+ * @author Ivelin Ivanov
+ * @description fetch current user if loggedin 
+ * @param {}
+ */
+
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+/**
+ * @author Ivelin Ivanov
+ * @description hit API to check user and get credits if any
+ * @param {string} token
+ */
+export const handleToken = (token) => async dispatch => {
+   const res = await axios.post('/api/stripe' ,token);
+
+   dispatch({type: FETCH_USER, payload: res.data});
 };
