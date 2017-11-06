33c1a527f9bc275c952c1164493358742a50cae3
Ivelin Ivanov <ivelin.ivanovxps@gmail.com>
2017-11-06 22:48:03 +0100

    added billing routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 require('./models/user'); // load the collection before calling Passport !
 require('./services/passport');
@@ -10,7 +11,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT;
 
--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 // schema for an instance of user 
 const userSchema = new Schema({
-    googleId: String
+    googleId: String,
+    credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema); // new collection in MongoDB called users
